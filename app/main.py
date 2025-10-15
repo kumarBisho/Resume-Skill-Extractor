91bf6f4e9b2ab9fa669329fd417d6f7f37725950
Bisho Kumar <kumarbisho02@gmail.com>
2025-10-15 13:14:30 +0530

    set environment variable properly

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ import os
 from datetime import datetime
 
 
+
 def create_app():
     app = Flask(__name__, template_folder='../templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///resumes.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL', 'sqlite:///resume.db')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
